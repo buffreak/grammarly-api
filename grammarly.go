c3033639026661f31c5c97e39908be1eb2cdaf9a
buffreak <revihammer@gmail.com>
2023-08-13 20:34:45 +0700

    fix error concurrent

--- grammarly.go
+++ grammarly.go
@@ -3,12 +3,20 @@ package grammarly
 import (
 	"encoding/json"
 	"fmt"
+	"math"
+	"math/rand"
 	"net/http"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/gorilla/websocket"
+)
+
+var (
+	ReadResponseErr error
 )
 
 type Configuration struct {
@@ -29,6 +37,8 @@ type GrammarlyParts struct {
 	Meta struct {
 		Label string `json:"label"`
 	} `json:"meta"`
+	TextColor string   `json:"textColor"`
+	Format    []string `json:"format"`
 }
 
 type GrammarlyLeftOrRight struct {
@@ -44,6 +54,7 @@ type GrammarlyLeftOrRight struct {
 type GrammarlyResponse struct {
 	MessageId     string                 `json:"messageId"`
 	OutcomeScores map[string]interface{} `json:"outcomeScores"`
+	ScoresStatus  string                 `json:"scoresStatus"`
 	Sdui          struct {
 		Child struct {
 			Child struct {
@@ -61,6 +72,12 @@ type GrammarlyResponse struct {
 			} `json:"child"`
 		} `json:"child"`
 	} `json:"sdui"`
+}
+
+type Correction struct {
+	Text         string
+	DeletedText  map[string]string
+	InsertedText map[string]string
 }
 
 func (gws *GrammarlyWS) SetCookieFile(filename string) error {
@@ -84,48 +101,56 @@ func (gws *GrammarlyWS) ConnectWS() error {
 		return fmt.Errorf("error connecting to ws grammarly: %+v", err)
 	}
 	gws.Ws = ws
+	gws.Ws.SetReadLimit(int64(math.Pow(2, 32)))
 	gws.Response = make(chan string)
 	return nil
 }
 
 func (gws *GrammarlyWS) WriteRequest(text string) error {
+
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	p1 := random.Intn(100) + 1
+	p2 := random.Intn(200-101+1) + 101
+
 	var params []string = []string{
-		`{"id":0,"action":"start","client":"extension_firefox","clientSubtype":"inline","clientVersion":"8.906.0","dialect":"american","docid":"f10feea2-0451-697f-fd71-27fbacd3cbbc","extDomain":"translate.google.co.id","documentContext":{},"clientSupports":["free_clarity_alerts","readability_check","filler_words_check","sentence_variety_check","vox_check","text_info","free_occasional_premium_alerts","set_goals_link","reconnect","gap_transform_card","tone_cards","user_mutes","mute_quoted_alerts","alerts_changes","ideas_gallery_link","full_sentence_rewrite_card","alerts_update","enclosing_highlight","realtime_proofit","tone_slider_card","ethical_ai_card","shorten_it","enclosing_highlight","main_start_highlight","consistency_check","super_alerts","suggested_snippets","autoapply"],"isDemoDoc":false,"sdui":{"supportedComponents":["alertsCount","alternativeChoice","alternativeSlider","applyAlerts","assistantCard","assistantFeed","behavior:strongAlertRef","block","box","button","clickableText","closeCard","column","dropDownMenuButton","focusAssistantCard","gButton","hideHighlights","highlightAlert","icon","image","inlineCard","list","nativeExperimentalGBConsistencyUpsellFooter","nativeExperimentalGBToneInsightsUpsellFooter","nativeFeedbackModal","nativeGetStartedChecklistModal","nativeInlineCardContent","nativeLearnMoreModal","nativeProofitModal","nativeSettingsModal","nativeToneInsightsModal","nextCard","notify","openCreateSnippetModal","openFeedback","openLearnMore","openLink","openSettings","openToneDetector","popAssistantFeed","prevCard","proofitButton","pushAssistantFeed","removeAlerts","removeRoot","row","scroll","selectAlternative","showHighlights","slider","strongAlertRef","switchView","text","transition","upgradeToPremium","viewStack"],"protocol":"2","dslSchema":"4.18.1"},"containerType":"form field"}`,
-		`{"id":1,"action":"submit_ot","rev":0,"doc_len":0,"chunked":false,"timer":{"client_clock":249621,"id":"cd4eebc7-3408-4083-a2d9-5c0f23b8399f"},"deltas": [{"ops":[{"insert":"` + text + `"}]}]}`,
+		`{"id":` + strconv.Itoa(p1) + `,"action":"start","client":"extension_firefox","clientSubtype":"inline","clientVersion":"8.906.0","dialect":"american","docid":"f10feea2-0451-697f-fd71-` + strconv.Itoa(p1) + `fbacd3cbbc","extDomain":"translate.google.co.id","documentContext":{},"clientSupports":["free_clarity_alerts","readability_check","filler_words_check","sentence_variety_check","vox_check","text_info","free_occasional_premium_alerts","set_goals_link","reconnect","gap_transform_card","tone_cards","user_mutes","mute_quoted_alerts","alerts_changes","ideas_gallery_link","full_sentence_rewrite_card","alerts_update","enclosing_highlight","realtime_proofit","tone_slider_card","ethical_ai_card","shorten_it","enclosing_highlight","main_start_highlight","consistency_check","super_alerts","suggested_snippets","autoapply"],"isDemoDoc":false,"sdui":{"supportedComponents":["alertsCount","alternativeChoice","alternativeSlider","applyAlerts","assistantCard","assistantFeed","behavior:strongAlertRef","block","box","button","clickableText","closeCard","column","dropDownMenuButton","focusAssistantCard","gButton","hideHighlights","highlightAlert","icon","image","inlineCard","list","nativeExperimentalGBConsistencyUpsellFooter","nativeExperimentalGBToneInsightsUpsellFooter","nativeFeedbackModal","nativeGetStartedChecklistModal","nativeInlineCardContent","nativeLearnMoreModal","nativeProofitModal","nativeSettingsModal","nativeToneInsightsModal","nextCard","notify","openCreateSnippetModal","openFeedback","openLearnMore","openLink","openSettings","openToneDetector","popAssistantFeed","prevCard","proofitButton","pushAssistantFeed","removeAlerts","removeRoot","row","scroll","selectAlternative","showHighlights","slider","strongAlertRef","switchView","text","transition","upgradeToPremium","viewStack"],"protocol":"2","dslSchema":"4.18.1"},"containerType":"form field"}`,
+		`{"id":` + strconv.Itoa(p2) + `,"action":"submit_ot","rev":` + strconv.Itoa(p1) + `,"doc_len":0,"chunked":false,"timer":{"client_clock":249621,"id":"cd4eebc7-3408-4083-a2d9-5c0f23b83` + strconv.Itoa(p2) + `f"},"deltas": [{"ops":[{"insert":"` + text + `"}]}]}`,
 	}
 
 	gws.Text = text
 	for key, param := range params {
-		err := gws.Ws.WriteMessage(websocket.TextMessage, []byte(param))
+		var body interface{}
+		err := json.Unmarshal([]byte(param), &body)
+		if err != nil {
+			return fmt.Errorf("error parse request message to ws grammarly at index [%d]: %+v", key, err)
+		}
+		err = gws.Ws.WriteJSON(body)
 		if err != nil {
 			return fmt.Errorf("error send message to ws grammarly at index [%d]: %+v", key, err)
 		}
 	}
+
 	return nil
 }
 
-func (gws *GrammarlyWS) readResponse() error {
+func (gws *GrammarlyWS) ParseResponse() (string, error) {
+	regexRemoveMultiSpace := regexp.MustCompile(`( {2,})`)
 	for {
 		_, msg, err := gws.Ws.ReadMessage()
 		if err != nil {
-			return fmt.Errorf("error read message to ws grammarly: %+v", err)
+			fmt.Printf("error read response grammarly: %s\n", err.Error())
+			break
 		}
-		gws.Response <- string(msg)
-	}
-}
-
-func (gws *GrammarlyWS) ParseResponse() (string, error) {
-	defer gws.Ws.Close()
-	go gws.readResponse()
-	for {
 		var grammarlyResp = GrammarlyResponse{}
-		buffer := <-gws.Response
+		buffer := string(msg)
+		fmt.Printf("%s\n", buffer)
 		if err := json.Unmarshal([]byte(buffer), &grammarlyResp); err != nil {
 			fmt.Printf("error parse response ws from grammarly: %+v\n", err)
 			continue
 		}
 
-		if len(grammarlyResp.OutcomeScores) > 0 {
+		if len(grammarlyResp.OutcomeScores) > 0 || grammarlyResp.ScoresStatus == "TOO_SMALL" {
 			break
 		}
 
@@ -144,23 +169,33 @@ func (gws *GrammarlyWS) ParseResponse() (string, error) {
 								}
 							}
 							regexRemoveChar := regexp.MustCompile(`…`)
-							var startText string = regexRemoveChar.ReplaceAllString(listElement[0].Text, "")
-							var endText string = regexRemoveChar.ReplaceAllString(listElement[len(listElement)-1].Text, "")
-							var regexInlineWord *regexp.Regexp = regexp.MustCompile(startText + `(.*?)` + endText)
-							var replacement string
-							for i := 1; i < len(listElement)-1; i++ {
-								if regexp.MustCompile("(?mi)^insert line break").MatchString(listElement[i].Meta.Label) {
-									if gws.WithNewline {
-										replacement = gws.NewlineOverride
+							var correction = Correction{}
+							correction.InsertedText = make(map[string]string)
+							correction.DeletedText = make(map[string]string)
+							var sequence string
+							for i := 0; i < len(listElement); i++ {
+								if listElement[i].Meta.Label == "" && listElement[i].Text != " " && listElement[i].TextColor == "CoreNeutral90" {
+									correction.Text += regexRemoveChar.ReplaceAllString(listElement[i].Text, "")
+
+									if i == len(listElement)-1 || i == len(listElement)-2 {
+										sequence += regexRemoveChar.ReplaceAllString(listElement[i].Text, "")
+									} else {
+										sequence += regexRemoveChar.ReplaceAllString(listElement[i].Text, "") + "(.*?)"
 									}
+								}
+
+								if regexp.MustCompile("(?mi)^insert line break").MatchString(listElement[i].Meta.Label) {
+									continue
 								} else if regexp.MustCompile("(?mi)^(insert) ").MatchString(listElement[i].Meta.Label) {
-									replacement = listElement[i].Text
+									correction.InsertedText[listElement[i].Meta.Label] = listElement[i].Text
+									correction.Text += listElement[i].Text
 								} else if regexp.MustCompile("(?mi)^(delete|remove) ").MatchString(listElement[i].Meta.Label) {
-									replacement = ""
+									correction.DeletedText[listElement[i].Meta.Label] = listElement[i].Text
+									correction.Text = strings.ReplaceAll(correction.Text, listElement[i].Text, " ")
 								}
 							}
-							regexRemoveMultiSpace := regexp.MustCompile(`( {2,})`)
-							gws.Text = regexRemoveMultiSpace.ReplaceAllString(regexInlineWord.ReplaceAllString(gws.Text, startText+replacement+endText), " ")
+							gws.Text = regexRemoveMultiSpace.ReplaceAllString(regexp.MustCompile(sequence).ReplaceAllString(gws.Text, correction.Text), " ")
+							gws.Text = regexp.MustCompile(`([a-zA-Z0-9])[.,]([a-zA-Z0-9])`).ReplaceAllString(gws.Text, `$1 $2`)
 						}
 					}
 				}
